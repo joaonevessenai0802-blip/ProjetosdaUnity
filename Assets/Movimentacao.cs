34ec492479a654ded42c762bae0d2c5708c6ccaa
joaonevessenai0802-blip <joaonevessenai0802@gmail.com>
2026-03-02 11:30:48 -0300

    movimentação

--- Assets/Movimentacao.cs
+++ Assets/Movimentacao.cs
@@ -5,12 +5,28 @@ public class Movimentacao : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(-2, 0, 0); 
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-0.05f, 0, 0);
+        if(Input.GetKey("d"))
+        {
+            transform.Translate(-0.05f, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(0.05f, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0,0.05f, 0);
+        }
+        if (Input.GetKey("x"))
+        {
+            transform.Translate(0,-0.05f, 0);
+        }
+        Physics.CheckSphere(transform.position, 1f);
     }
 }
